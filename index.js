d9a4ae627d7d7f3e834ec814c5e935f2fd11b9d8
sam <sam@mongrov.com>
2019-03-06 20:33:52 +0530

    add dependency of application

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import Config from './config';
+import Application from './config';
 import Errors from './errors';
 import Navigation from './navigation';
 
 module.exports = {
+  Application,
   Config,
   Errors,
   Navigation,
